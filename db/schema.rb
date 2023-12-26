7333a65169dbcdd77a9f83a998d3aaed7c40e836
zerubabel Filmon <zerubabelfilmon1@gmail.com>
2023-12-26 10:36:27 -1200

    Abdisa wolde

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_26_215223) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_26_223415) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
